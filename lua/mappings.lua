3214768d7a446af687053d90ab23607ab975e233
argonai <aaronvdw@protonmail.com>
2024-04-06 02:09:14 +0200

    almost there (see print statement)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,9 +1,9 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
 local map = vim.keymap.set
-local harpoon = require("harpoon")
+local harpoon = require "harpoon"
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
@@ -14,24 +14,57 @@ map("i", "jk", "<ESC>")
 
 local conf = require("telescope.config").values
 local function toggle_telescope(harpoon_files)
-    local file_paths = {}
+  local make_finder = function()
+    local paths = {}
     for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
+      table.insert(paths, item.value)
     end
 
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
+    return require("telescope.finders").new_table {
+      results = paths,
+    }
+  end
+
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = make_finder(),
+      previewer = conf.file_previewer {},
+      sorter = conf.generic_sorter {},
+      attach_mappings = function()
+        map(
+          "i",
+          "<A-d>", -- your mapping here
+          function()
+            local state = require "telescope.actions.state"
+            local selected_entry = state.get_selected_entry()
+            print(vim.inspect(selected_entry.index))
+            harpoon:list():removeAt(selected_entry.index)
+          end
+        )
+
+        return true
+      end,
+    })
+    :find()
 end
 
-map("n", "<leader>pa", function() harpoon:list():add() end, {desc = "Add file to harpoon"})
-map("n", "<leader>pe", function () toggle_telescope(harpoon:list()) end, {desc = "Open harpoon window"})
-map("n", "<leader>p1", function () harpoon:list():select(1) end, {desc = "Select first item in harpoon list"})
-map("n", "<leader>p2", function () harpoon:list():select(2) end, {desc = "Select second item in harpoon list"})
-map("n", "<leader>p3", function () harpoon:list():select(3) end, {desc = "Select third item in harpoon list"})
-map("n", "<leader>p4", function () harpoon:list():select(4) end, {desc = "Select fourth item in harpoon list"})
+map("n", "<leader>pa", function()
+  harpoon:list():add()
+end, { desc = "Add file to harpoon" })
+map("n", "<leader>pe", function()
+  toggle_telescope(harpoon:list())
+end, { desc = "Open harpoon window" })
+
+map("n", "<leader>p1", function()
+  harpoon:list():select(1)
+end, { desc = "Select first item in harpoon list" })
+map("n", "<leader>p2", function()
+  harpoon:list():select(2)
+end, { desc = "Select second item in harpoon list" })
+map("n", "<leader>p3", function()
+  harpoon:list():select(3)
+end, { desc = "Select third item in harpoon list" })
+map("n", "<leader>p4", function()
+  harpoon:list():select(4)
+end, { desc = "Select fourth item in harpoon list" })
